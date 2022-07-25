3c0e45eff237d2414fa7f7f6914cf9f7a57a2a33
R4chel <rae84@cornell.edu>
2022-07-25 15:00:28 -0400

    play sound and light up on key press

--- explore.py
+++ explore.py
@@ -7,14 +7,24 @@ Simpletest demo for MacroPad. Prints the key pressed, the relative position of t
 encoder, and the state of the rotary encoder switch to the serial console.
 """
 import time
+from rainbowio import colorwheel
 from adafruit_macropad import MacroPad
 
 macropad = MacroPad()
 
+tones = [196, 220, 246, 262, 294, 330, 349, 392, 440, 494, 523, 587]
+
+debug = False
 while True:
     key_event = macropad.keys.events.get()
     if key_event and key_event.pressed:
         print("Key pressed: {}".format(key_event.key_number))
-    print("Encoder: {}".format(macropad.encoder))
-    print("Encoder switch: {}".format(macropad.encoder_switch))
-    time.sleep(0.4)
+        macropad.pixels[key_event.key_number] = colorwheel(
+                int(255 / 12) * key_event.key_number
+            )
+        macropad.play_tone(tones[key_event.key_number], 0.5)
+    else:
+            macropad.pixels.fill((0, 0, 0))
+    if debug:
+        print("Encoder: {}".format(macropad.encoder))
+        print("Encoder switch: {}".format(macropad.encoder_switch))
