a6145c6feb26b6eec3210710564fe0458f46f408
R4chel <rae84@cornell.edu>
2022-07-28 16:08:16 -0400

    write more messages

--- wade.py
+++ wade.py
@@ -10,21 +10,37 @@ import usb_cdc
 import json
 import io
 
-print( "hello")
-
-serial = usb_cdc.data
-print(serial.connected)
-print("here")
+debug = true
 
 # For most CircuitPython boards
 
 
+def write_msg(serial, msg):
+    message = json.dumps(msg)
+    serial.write(bytes(f"{message}\r\n", "utf-8"))
+    serial.flush()
+
+serial = usb_cdc.data
 macropad = MacroPad()
+encoder_value = macropad.encoder
+encoder_switch_value = macropad.encoder_switch
+
 while True:
     key_event = macropad.keys.events.get()
     if key_event and key_event.pressed:
-        print("Key pressed: {}".format(key_event.key_number))
-        message = json.dumps({"key": key_event.key_number})
-        serial.write(bytes(f"{message}\r\n", "utf-8"))
-        # serial.write("\r\n")
-        serial.flush()
+        message = {"key": key_event.key_number}
+        write_msg(serial, message)
+        if debug:
+            print("Key pressed: {}".format(key_event.key_number))
+    if encoder_switch_value != macropad.encoder_switch:
+        encoder_switch_value = macropad.encoder_switch
+        message = {"encoder_switch": encoder_switch_value}
+        write_msg(serial, message)
+        if debug:
+            print("Encoder switch: {}".format(macropad.encoder_switch))
+    if encoder_value != macropad.encoder_switch:
+        encoder_value = macropad.encoder_switch
+        message = {"encoder": encoder_switch_value}
+        write_msg(serial, message)
+        if debug:
+            print("Encoder: {}".format(macropad.encoder))
