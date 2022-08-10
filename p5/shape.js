25726bffb04576a49b9f006d9c63225619713ffd
R4chel <rae84@cornell.edu>
2022-08-10 14:38:14 -0400

    whoops never commmited shape file

---
+++ p5/shape.js
@@ -0,0 +1,66 @@
+function Shape({
+    center,
+    radius,
+    noise,
+    numPoints,
+    color,
+
+}) {
+    this.center = center;
+    this.radius = radius;
+    this.color = color;
+    this.noise = noise;
+    this.points = [];
+
+
+    this.onCreation = function() {
+        for (let i = 0; i < numPoints; i++) {
+            this.points.push(
+                { r : randomGaussian(0, this.noise) }
+               
+
+            )
+        }
+
+    }
+
+    this.draw = function() {
+        stroke(toColor(this.color));
+        //fill(toColor(this.color));
+        beginShape();
+        for (let i = 0; i < numPoints; i++) {
+            let theta = i * 2 * PI / numPoints;
+            let p = this.points[i];
+            let x = cos(theta) * (this.radius + p.r) + this.center.x;
+            let y = sin(theta) * (this.radius + p.r) + this.center.y;
+            curveVertex(x, y);
+        }
+        endShape(CLOSE);
+
+    }
+
+    this.update = function(canvas){
+        for (let i = 0; i < numPoints; i++) {
+            
+            let p = this.points[i];
+            let update = randomGaussian(this.radius - p.r, this.noise);
+            p.r += update;
+            if(p.r + this.radius < 5){
+                p.r += 5;
+            }
+        }
+
+        let center_x_update = randomGaussian(0, this.noise /2);
+        let center_y_update = randomGaussian(0, this.noise /2 );
+        this.center.x = constrain(center.x+center_x_update, 0, canvas.width);
+        this.center.y = constrain(center.y+center_y_update, 0, canvas.height);
+
+    } 
+    
+    this.onCreation();
+
+}
+
+function toColor(x) {
+    return color(x.r, x.g, x.b);
+}
